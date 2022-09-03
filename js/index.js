cf0c5476cbfce1ee7f03ef9d40e904961cdda999
noman2047 <nomancse101@gmail.com>
2022-09-03 18:49:51 +0600

    Create few option

--- js/index.js
+++ js/index.js
@@ -1,1 +1,92 @@
-console.log("Welcome to news protal")
+// All news 
+const url='https://openapi.programming-hero.com/api/news/categories'
+fetch(url)
+.then(req=>req.json())
+.then(allnews=>allNewsCata(allnews.data.news_category))
+
+const allNewsCata = allnews=>{
+  for(let eachNewsCat of allnews){
+    const ul1=document.getElementById('ul1');
+    const li=document.createElement('li');
+    li.classList.add('nav-item');
+    li.innerHTML=`
+    <a class="nav-link" href="#" id="${eachNewsCat.category_id}" onclick="eachButton('${eachNewsCat.category_id}')">${eachNewsCat.category_name}</a>
+    `
+    ul1.appendChild(li);
+  }
+}
+function eachButton(id){
+  console.log(id)
+  const idurl=`https://openapi.programming-hero.com/api/news/category/${id}`
+  fetch(idurl)
+  .then(req1=>req1.json())
+  .then(eachNews=>eachNewsPortal(eachNews.data))
+}
+
+const eachNewsPortal= eachNews=>{
+  const footer=document.getElementById('footer');
+  // Take Found 
+  const found=document.getElementById('found');
+  found.innerHTML=`
+  <h4>${eachNews.length} items found for category Entertainment</h4>`
+
+  if(eachNews.length== 0){
+    footer.classList.add('d-none');
+  }
+  else{
+    footer.classList.remove('d-none')
+  }
+  const newShow=document.getElementById('newShow');
+  newShow.innerHTML='';
+  for(let eachNewshow of eachNews){
+  const div=document.createElement('div');
+  const newShow=document.getElementById('newShow');
+  div.innerHTML=`
+  <div class="card mb-3" style="max-width:100%;">
+    <div class="row g-0">
+      <div class="col-lg-4 col-md-4 col-sm-4">
+         <img src="${eachNewshow.thumbnail_url}" class="img-fluid rounded-start p-4" alt="...">
+      </div>
+      <div class="col-lg-8 col-md-8 col-sm-8">
+        <div class="card-body">
+        <h5 class="card-title mt-5">${eachNewshow.title}</h5> 
+          <p class="card-text">${eachNewshow.details.slice(1,350)}.....</p>
+          <div class="comma-star d-flex justify-content-between mt-3">
+              <div class="comma">
+                <div class="feedback d-flex gap-3">
+                  <div class="img-t">
+                    <img style="height:50px;width: 50px;" class="image-fluid border rounded-circle"  src="${eachNewshow.author.img}" alt="">
+                  </div>
+                  <div class="name-d">
+                    <p><span class="fw-bold">${eachNewshow.author.name}</span><span class="d-block fdasig">${eachNewshow.author.published_date}</span></p>
+                  </div>
+                </div>
+              </div>
+              <div>
+              <i class="fa-regular fa-eye me-2"></i>
+              <i class="fs-5 fs-bold fw-bold">${eachNewshow.total_view}</i>
+              </div>
+              <div class="star text-warning">
+                <i>${eachNewshow.rating.number}</i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star-of-david"></i> 
+              </div>
+              <div>
+              <i class="fa-sharp fa-solid fa-arrow-right-long text-danger fw-bold fs-5 onclick="clickArrowButton()"></i>
+              </div>
+           </div>
+ 
+
+      </div>
+      </div>
+    </div>
+  </div>`
+newShow.appendChild(div);
+} 
+}
+function clickArrowButton(){
+  console.log("click")
+}
